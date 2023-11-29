58e0904644ae482d81e23f3202e7f6e3bcc5f8f6
DiegoGuerraA00835430 <88684991+DiegoGuerraA00835430@users.noreply.github.com>
2023-11-28 18:46:30 -0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'itesm',
+  password: '352641Dg',
   database: 'crazysorteo_test'
 });
 
